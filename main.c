5f5c2a89f953cda5d1df6c629b495ebe9f264a0b
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-04-05 04:44:30 +0700

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int solve(int i, int j, int n, int arr[n][n]);
+int max(int a, int b);
+
 int main() {
-    // baca input
     int n;
     scanf("%d",&n);
     int arr[n][n];
@@ -19,7 +21,36 @@ int main() {
             scanf("%d", &arr[i][j]);
         }
     }
-    
-    // lanjutkan program
+    printf("%d\n",solve(0,0,n,arr));
+}
 
+int solve(int i, int j, int n, int arr[n][n]) {
+    int a,b,c,d;
+    a=0;b=0;c=0;d=0;
+    if(i-1>=0) {
+        if(arr[i-1][j]==1+arr[i][j]) {
+            a=solve(i-1,j,n,arr);
+        }
+    }
+    if(i+1<n) {
+        if(arr[i+1][j]==1+arr[i][j]) {
+            b=solve(i+1,j,n,arr);
+        }
+    } 
+    if(j-1>=0) {
+        if(arr[i][j-1]==1+arr[i][j]) {
+        c=solve(i,j-1,n,arr);
+        }
+    }
+    if(j+1<n) {
+        if(arr[i][j+1]==1+arr[i][j]) {
+            d=solve(i,j+1,n,arr);
+        }
+    }
+    return max(max(a,b),max(c,d))+1;
+}
+
+int max(int num1, int num2)
+{
+    return (num1 > num2 ) ? num1 : num2;
 }
